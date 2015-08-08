a57f39ade26a6a9103c8e29f1eea0a3dc689bcb2
richard <richard@richards-mini.lan>
2015-08-08 03:43:56 -0500

    1
    1

---
+++ dragonpoop_alpha/dragonpoop/renderer/renderer_model/renderer_models.h
@@ -0,0 +1,12 @@
+
+#ifndef dragonpoop_renderer_models_h
+#define dragonpoop_renderer_models_h
+
+#include "renderer_model.h"
+#include "renderer_model_ref.h"
+#include "renderer_model_readlock.h"
+#include "renderer_model_writelock.h"
+
+#include "renderer_model_instance/renderer_model_instances.h"
+
+#endif
