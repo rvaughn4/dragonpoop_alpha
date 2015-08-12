3e0fd9c030d4233be190f8ccc7e9a3ee7ac4a46f
richard <richard@richards-mini.lan>
2015-08-12 05:50:44 -0500

    2
    2

--- dragonpoop_alpha/dragonpoop/gfx/model/model_instance/model_instance_animation/model_instance_animation.cpp
+++ dragonpoop_alpha/dragonpoop/gfx/model/model_instance/model_instance_animation/model_instance_animation.cpp
@@ -1,13 +1,21 @@
 
 #include "model_instance_animation.h"
+#include "../../model_animation/model_animation.h"
+#include "../../model_readlock.h"
+#include "../../../../core/dpthread/dpthread_lock.h"
+#include <list>
+#include "../../model_animation_frame/model_animation_frame.h"
 
 namespace dragonpoop
 {
     
     //ctor
-    model_instance_animation::model_instance_animation( dpid id ) : model_component( id, model_component_type_animation )
+    model_instance_animation::model_instance_animation( model_animation *m ) : model_component( m->getId(), model_component_type_animation )
     {
-        
+        this->bIsRepeat = m->isRepeated();
+        this->bIsAutplay = m->isAutoPlay();
+        this->repeat_delay_ms = m->getRepeatDelay();
+        this->start_time = this->end_time = 0;
     }
     
     //dtor
@@ -50,6 +58,96 @@ namespace dragonpoop
     unsigned int model_instance_animation::getRepeatDelay( void )
     {
         return this->repeat_delay_ms;
+    }
+    
+    //start animation
+    void model_instance_animation::start( dpthread_lock *thd, model_readlock *ml )
+    {
+        std::list<model_animation_frame *> l;
+        std::list<model_animation_frame *>::iterator i;
+        model_animation_frame *p;
+        
+        this->start_time = thd->getTicks();
+        this->end_time = 0;
+        
+        ml->getComponentsByParent( model_component_type_animation_frame, this->getId(), (std::list<model_component *> *)&l );
+        
+        for( i = l.begin(); i != l.end(); ++i )
+        {
+            p = *i;
+            if( p->getTime() > this->end_time )
+                this->end_time = p->getTime();
+        }
+    }
+    
+    //stop animation
+    void model_instance_animation::stop( void )
+    {
+        this->start_time = this->end_time = 0;
+    }
+    
+    //returns true if playing
+    bool model_instance_animation::isPlaying( void )
+    {
+        return this->start_time != 0;
+    }
+    
+    //return ms played
+    uint64_t model_instance_animation::getPlayTime( dpthread_lock *thd )
+    {
+        uint64_t t, td;
+        
+        if( !this->start_time || !this->end_time )
+            return 0;
+            
+        t = thd->getTicks();
+        if( t > this->start_time )
+            td = t - this->start_time;
+        else
+            td = 0;
+        
+        if( td > this->end_time )
+        {
+            this->start_time = t + this->repeat_delay_ms - ( td - this->end_time );
+            if( t > this->start_time )
+                td = t - this->start_time;
+            else
+                td = 0;
+        }
+        
+        return td;
+    }
+    
+    //return closest frame after time
+    model_frame *model_instance_animation::getFrameAtTime( model_readlock *ml, uint64_t t )
+    {
+        std::list<model_animation_frame *> l;
+        std::list<model_animation_frame *>::iterator i;
+        model_animation_frame *p, *f;
+        
+        ml->getComponentsByParent( model_component_type_animation_frame, this->getId(), (std::list<model_component *> *)&l );
+        
+        f = 0;
+        for( i = l.begin(); i != l.end(); ++i )
+        {
+            p = *i;
+            
+            if( p->getTime() < t )
+                continue;
+            if( !f )
+            {
+                f = p;
+                continue;
+            }
+            if( f->getTime() < p->getTime() )
+                continue;
+            f = p;
+        }
+        
+        if( !f )
+            return 0;
+        
+        return (model_frame *)ml->findComponent( model_component_type_frame, f->getFrameId() );
     }
     
 };
--- dragonpoop_alpha/dragonpoop/gfx/model/model_instance/model_instance_animation/model_instance_animation.h
+++ dragonpoop_alpha/dragonpoop/gfx/model/model_instance/model_instance_animation/model_instance_animation.h
@@ -7,6 +7,11 @@
 namespace dragonpoop
 {
     
+    class model_animation;
+    class model_readlock;
+    class dpthread_lock;
+    class model_frame;
+    
     class model_instance_animation : public model_component
     {
         
@@ -14,13 +19,14 @@ namespace dragonpoop
         
         bool bIsRepeat, bIsAutplay;
         unsigned int repeat_delay_ms;
+        uint64_t start_time, end_time;
         
     protected:
         
     public:
         
         //ctor
-        model_instance_animation( dpid id );
+        model_instance_animation( model_animation *m );
         //dtor
         virtual ~model_instance_animation( void );
         //set repeat mode
@@ -35,6 +41,16 @@ namespace dragonpoop
         void setRepeatDelay( unsigned int ms );
         //get delay between repeats
         unsigned int getRepeatDelay( void );
+        //start animation
+        void start( dpthread_lock *thd, model_readlock *ml );
+        //stop animation
+        void stop( void );
+        //returns true if playing
+        bool isPlaying( void );
+        //return ms played
+        uint64_t getPlayTime( dpthread_lock *thd );
+        //return closest frame after time
+        model_frame *getFrameAtTime( model_readlock *ml, uint64_t t );
         
     };
     
