be90e2d7fcb5b5a777c90c6d0315ff1fc47cc222
richard <richard@richards-mini.lan>
2015-10-20 13:23:10 -0500

    4
    4

---
+++ dragonpoop_alpha/dragonpoop/renderer/api_stuff/window/window.cpp
@@ -0,0 +1,43 @@
+
+#include "window.h"
+
+namespace dragonpoop
+{
+    
+    //ctor
+    window::window( float w, float h )
+    {
+        
+    }
+    
+    //dtor
+    window::~window( void )
+    {
+        
+    }
+    
+    //run window
+    void window::run( void )
+    {
+        
+    }
+    
+    //returns true if window is open
+    bool window::isOpen( void )
+    {
+        return 0;
+    }
+    
+    //returns window width
+    float window::getWidth( void )
+    {
+        return 0;
+    }
+    
+    //returns window height
+    float window::getHeight( void )
+    {
+        return 0;
+    }
+
+};
---
+++ dragonpoop_alpha/dragonpoop/renderer/api_stuff/window/window.h
@@ -0,0 +1,35 @@
+
+#ifndef dragonpoop_window_h
+#define dragonpoop_window_h
+
+namespace dragonpoop
+{
+    
+    class window
+    {
+        
+    private:
+        
+    protected:
+        
+    public:
+        
+
+        //ctor
+        window( float w, float h );
+        //dtor
+        virtual ~window( void );
+        //run window
+        virtual void run( void );
+        //returns true if window is open
+        virtual bool isOpen( void );
+        //returns window width
+        virtual float getWidth( void );
+        //returns window height
+        virtual float getHeight( void );
+        
+    };
+    
+};
+
+#endif
